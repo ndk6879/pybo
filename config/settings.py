b75a39c1179ba0d8278baea5d584161c6a9d120f
Donggyun Nam <ndk6879@naver.com>
2021-04-27 16:39:19 +0900

    ALLOWED_HOSTS 변경

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'y2b1ydyu-evcut-t6)0m()h$7ogfw!7h5g7hm#&1igv2394zt)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.79.197.129']
 
 
 # Application definition

e5ef3c6d0327cd66ffb9bd14636f389acd23de4f
Donggyun Nam <ndk6879@naver.com>
2021-05-03 14:16:38 +0900

    Deal with 404 error

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,3 +3,4 @@ from .base import *
 ALLOWED_HOSTS = ['52.79.197.129']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
+DEBUG = False
--- config/urls.py
+++ config/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
     path('common/', include('common.urls')),
-
 ]   
+
+handler404 = 'common.views.page_not_found'
